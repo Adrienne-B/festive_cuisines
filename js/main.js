f6fb56693ff3237e2cc2f2b2616e6cbf7bb3ddee
adrienne_b <WorldofArts_95@yahoo.com>
2021-07-09 12:43:19 -0500

    hamburger nav.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-var headingTop = document.querySelector("h1");
-console.log(headingTop);
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector(' .closeMenu');
+const openMenu = document.querySelector(' .openMenu');
 
 
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+
+function show() {
+    mainMenu.style.display ='flex';
+    mainMenu.style.top = '0';
+}
+
+function close() {
+    mainMenu.style.top ='-100%';
+}
 
 
 
