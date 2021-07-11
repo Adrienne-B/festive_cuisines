5b9c9c3b116aeebccc548851946714b4d3f8bfb3
adrienne_b <WorldofArts_95@yahoo.com>
2021-07-11 18:12:46 -0500

    Aligning, change font size, hamburger nav...

---
+++ hamburger/js/javascript.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+
+function onClickMenu() {
+	
+	document.getElementById("menu").classList.toggle("change");
+	
+	document.getElementById("nav").classList.toggle("change";)
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg";)
+}
+
+
+
