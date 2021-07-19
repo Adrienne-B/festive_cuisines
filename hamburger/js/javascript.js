9fa1973a81e6efd56a0855e15555426e52bc2282
adrienne_b <WorldofArts_95@yahoo.com>
2021-07-19 10:06:59 -0500

    commented out hamburger css.

--- hamburger/js/javascript.js
+++ hamburger/js/javascript.js
@@ -5,10 +5,22 @@ function onClickMenu() {
 	
 	document.getElementById("menu").classList.toggle("change");
 	
-	document.getElementById("nav").classList.toggle("change";)
+	document.getElementById("nav").classList.toggle("change");
 	
-	document.getElementById("menu-bg").classList.toggle("change-bg";)
+	document.getElementById("menu-bg").classList.toggle("change-bg");
 }
 
+var button = document.querySelector(" #menu-bar");
+var modal = document.querySelector("nav");
+
+
+nav.addEventListener("click", function () {
+  modal.classList.add(".menu-bar");
+
+});
+
+modalX.addEventListener("click", function () {
+  modal.classList.remove("nav");
+});
 
 
